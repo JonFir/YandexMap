83efcba69a298796ba1780aeca1c7726ae2d0043
JonFir <yolchev@gmail.com>
2016-12-26 21:46:10 +0700

    Initial Commit

---
+++ YandexMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YandexMap
+//
+//  Created by rsi-pc on 26.12.16.
+//  Copyright © 2016 pinspb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
